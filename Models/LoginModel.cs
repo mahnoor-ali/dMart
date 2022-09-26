82e3cdc4e796a9b20651ec4b2d95ded36e7416e5
Mahnoor <mahnoorali57se@gmail.com>
2022-09-26 19:50:37 +0500

    login model added

---
+++ Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMART.Models
+{
+    public class LoginModel
+    {
+
+        [Required(ErrorMessage = " Email field can't be empty")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(15, ErrorMessage = "Must be between 6 and 15 characters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
